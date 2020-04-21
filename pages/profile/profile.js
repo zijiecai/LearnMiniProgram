7ef7fe5dfead594bd4d6a792fcd6d3e518c8e34d
zijiecai <13005338063@163.com>
2020-04-21 14:30:55 +0800

    注册小程序和页面

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,32 +1,11 @@
-// pages/home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name:'菜籽',
-    age:18,
-    students:[
-      {id:1,name:'kobe',age:30},
-      {id:2,name:'james',age:28},
-      {id:3,name:'curry',age:32},
-      {id:4,name:'caizi',age:18},
-    ],
-    counter:0,
-  },
-  handleBtnClick() {
-    this.setData({
-      counter: this.data.counter+1
-    })
-  },
-  handleSubtraction(){
-    this.setData({
-      counter:this.data.counter-1
-    })
-  },
-  handleGetUserInfo(event){
-    console.log(event);
+
   },
 
   /**
@@ -64,24 +43,18 @@ Page({
 
   },
 
-  // 4.其他事件
-  // 监听页面滚动
-  onPageScroll(obj){
-    // console.log(obj);
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('下拉刷新事件触发');
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log('页面滚动到底部');
+
   },
 
   /**
