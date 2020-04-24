fcbad067a1dcecb1191704fc3681b13911d9449e
zijiecai <13005338063@163.com>
2020-04-24 17:51:01 +0800

    1111_组件化开发

---
+++ components/my-prop/my-prop.js
@@ -0,0 +1,32 @@
+// components/my-prop/my-prop.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:'我是默认的标题',
+      // 观察者
+      observer:function(newVal,oldVal){
+        console.log(newVal,oldVal);
+      }
+    }
+  },
+  // 传入的样式
+  externalClasses:['titleclass'],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    counter:0,
+    isShow:true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleIncrement(event){
+    console.log('------------------',event);
+    this.setData({
+      counter:this.data.counter+1
+    })
+    console.log(event.detail.name,event.detail.age);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTabClick(event){
+    console.log(event);
   },
+  handleIncrementCpn(){
+    // 最终目的：修改my-sel组件中的数据counter
+    // 1.获取组件对象
+    const my_sel = this.selectComponent('#sel-id');
+    // console.log(my_sel);
+    // 2.通过setData修改组件中的数据(不合理)
+    // my_sel.setData({
+    //   counter:my_sel.data.counter + 20
+    // })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    // 3.通过方法修改组件中的数据
+    my_sel.incrementCounter(10);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChangeShow(){
+    this.setData({
+      isShow:!this.data.isShow
+    })
   }
 })
