07d6d8cdfd781eb7d83b08294829fb7d4e40ead2
zijiecai <13005338063@163.com>
2020-04-25 13:11:06 +0800

    12_网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js';
+
 Page({
 
   /**
@@ -12,7 +14,56 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 1.原生的方式发送网络请求
+    this.get_data_origin();
 
+    // 2.使用封装的request发送网络请求
+    // Promise最大的好处就是防止出现回调地狱
+    request({
+      url:'http://152.136.185.210:8000/api/h8/recommend'
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  },
+
+  get_data_origin(){
+    // 发送网络请求
+    // 1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/h8/recommend',
+    //   success:function(res){
+    //     console.log(res);
+    //   }
+    // })
+    // 2.发送get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/h8/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1
+    //   },
+    //   success:function(res){
+    //     console.log(res);
+    //   }
+    // })
+
+    // 3.发送post请求，并且携带参数
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method: 'post',
+      data: {
+        name: 'caizi',
+        age: 18
+      },
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (err) {
+        console.log(err);
+      }
+    })
   },
 
   /**
