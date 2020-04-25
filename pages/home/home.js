3f41aac7080bb3bd8768a8937cc9c7beacbb1010
zijiecai <13005338063@163.com>
2020-04-25 14:28:54 +0800

    13_展示弹窗与分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,66 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中ing',
+      duration:3000,
+      icon:'loading',
+      // image:'/assets/icon/icon.png'
+      // 是否显示透明层，防止触摸穿透
+      mask:true,
+      success:function(){
+        console.log('展示弹窗成功')
+      },
+      fail:function(){
+        console.log('展示弹窗失败')
+      },
+      complete:function(){
+        console.log('完成函数的调用')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈',
+      // showCancel:false,
+      cancelText:'退出',
+      cancelColor:'red',
+      success:function(res){
+        console.log(res);
+        if(res.cancel){
+          console.log('用户点击了取消按钮');
+        }
+        if(res.confirm){
+          console.log('用户点击了确定按钮');
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    })
+    setTimeout(()=> {
+      // 必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor:'skyblue',
+      success:function(res){
+        console.log(res);
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return{
+      title:'你好啊 菜籽jie',
+      path:'/pages/about/about',
+      imageUrl:'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }
   }
 })
